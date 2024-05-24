b263f46f68b5000fd8c2ae1fde2fbb5aa25e7a2d
Gajanan <palankar.gajanan@csscorp.com>
2024-05-24 21:54:45 +0530

    Routing added with changes in folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { FooterComponent } from "./components/layout/footer/footer.component";
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
