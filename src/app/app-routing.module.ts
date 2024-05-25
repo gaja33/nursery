b0227b39557fde152a78836ead0d3bbd24fe1cc1
Gajanan <palankar.gajanan@csscorp.com>
2024-05-25 16:03:02 +0530

    Play zone component implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/pages/home/home.component";
 import { AboutUsComponent } from "./components/pages/about-us/about-us.component";
 import { PageNotFoundComponent } from "./components/pages/page-not-found/page-not-found.component";
+import { PlayZoneComponent } from "./components/pages/play-zone/play-zone.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "about-us", component: AboutUsComponent },
+  { path: "play-zone", component: PlayZoneComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AboutUsComponent } from './components/pages/about-us/about-us.component
 import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
 import { EnrollmentComponent } from './components/shared/enrollment/enrollment.component';
 import { TestimonialComponent } from './components/shared/testimonial/testimonial.component';
+import { PlayZoneComponent } from './components/pages/play-zone/play-zone.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent, PageNotFoundComponent, EnrollmentComponent, TestimonialComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent, PageNotFoundComponent, EnrollmentComponent, TestimonialComponent, PlayZoneComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
