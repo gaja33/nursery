cb6d9caede333d62cf4197e53536a63c7468bfd0
Gajanan <palankar.gajanan@csscorp.com>
2024-05-25 13:41:39 +0530

    About us implemented

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from "@angular/core";
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    (function ($) {
+      //sticky menu
+      $(window).on("scroll", function () {
+        var window_top = $(window).scrollTop() + 1;
+        if (window_top > 50) {
+          $(".header").addClass("menu_fixed animated fadeInDown");
+        } else {
+          $(".header").removeClass("menu_fixed animated fadeInDown");
+        }
+      });
+    })(jQuery);
   }
-
 }
