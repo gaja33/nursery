b01de5e82479c593451523008ffa6a9fb1523f7a
Gajanan <palankar.gajanan@csscorp.com>
2024-05-25 12:08:20 +0530

    Home page implemented with adding owl carousel library

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from "@angular/core";
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    (function ($) {
+      $(document).ready(function () {
+        var bannerSlider = $(".bannerslider");
+        if (bannerSlider.length) {
+          bannerSlider.owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            navText: [
+              "<i class='fa fa-chevron-left'></i>",
+              "<i class='fa fa-chevron-right'></i>",
+            ],
+            dots: false,
+            autoplay: true,
+            autoplayHoverPause: true,
+            smartSpeed: 800,
+            autoplayTimeout: 3000,
+            responsive: {
+              0: {
+                nav: false,
+              },
+              768: {
+                nav: true,
+              },
+            },
+          });
+        }
+      });
+    })(jQuery);
   }
-
 }
